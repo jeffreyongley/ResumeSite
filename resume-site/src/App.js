947aae71aec77ffd26ed0fa303bf0dced2cfcce1
Jeffrey Ongley <jongley@altuscampus.com>
2025-01-18 14:00:41 -0800

    basic setup, Hello World

---
+++ resume-site/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Jeffrey Ongley</h1>
+        <h4>Software Engineer</h4>
+        <p>
+          Hello World
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
