a63f6750f0f346b11b6941d6511f4f31d44e19a7
Jeffrey Ongley <jongley@altuscampus.com>
2025-01-18 15:39:12 -0800

    created basic setup of the main Page

--- resume-site/src/App.js
+++ resume-site/src/App.js
@@ -1,20 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Button = ({ children, onClick }) => {
+  return (
+    <button class='menu-button' onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Jeffrey Ongley</h1>
-        <h4>Software Engineer</h4>
-        <p>
-          Hello World
-        </p>
-
+        
       </header>
+      <body className="App-body" >
+        <div className="name">Jeffrey Ongley</div>
+        <div className="title">Software Engineer</div>
+        <div className="picture"><img src={logo} className='App-logo' alt="portrait"></img></div>
+        <div className="menu">
+          <Button>Biography</Button>
+          <Button>Work Experience</Button>
+          <Button>Education</Button>
+          <Button>Languages and Technologies</Button>
+        </div>
+      </body>
     </div>
   );
 }
+
+
 
 export default App;
