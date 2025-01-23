698c8571d3bf03882cf263c5a2a23b232a9ba52b
Jeffrey Ongley <jongley@altuscampus.com>
2025-01-23 13:05:50 -0800

    Finished first =version of education page

--- resume-site/src/components/Education.jsx
+++ resume-site/src/components/Education.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import csusb from '../media/csusb.jpg'
 import '../css/Education.css';
+import { useNavigate } from "react-router-dom";
+
+
+const Button = ({ children, onClick }) => {
+    return (
+    <button className='menu-button' onClick={onClick}>
+        {children}
+    </button>
+    );
+};
 
 function Education() {
-    // return (
-    //     <div className="App">
-    //         <div className="App-body">
-    //             <div className="degree">Bachelor of Science Degree in Computer Science </div>
-    //             <div className="description">
-    //                 I earned my Bachelor of Science in Computer Science from California State University, San Bernardino in May 2021. During my time at CSUSB, I completed a diverse range of courses, including Software Engineering, Advanced Algorithms, Introduction to Machine Learning, and Artificial Intelligence. These courses provided hands-on experience with various programming languages, such as Python, JavaScript, and C++.
-    //             </div> 
-    //             <div className='picture'><img src={csusb} alt="CSUSB" className="csusb-image" /></div>
-    //         </div>
-    //     </div>
-    // )
+
+    const navigate = useNavigate();
 
     return (
-        <div className="App">
-            <div className="App-body" >
-            <div className="degree">Jeffrey Ongley</div>
-            <div className="descrip">Software Engineer</div>
-            {/* <div className="picture"><img src={logo} className='App-logo' alt="portrait"></img></div> */}
-            
+        <div className="App" style={{ justifyContent: 'space-evenly' }}>
+            <div className="name">California State University - San Bernardino</div>
+            <div className="title">B.S. in Computer Science, Class of 2021</div>
+            <div className="csusb-container"><img src={csusb} className='csusb' alt="portrait"></img></div>            
+            <div className="description">
+                I earned my Bachelor of Science in Computer Science from California State University, San Bernardino in May 2021. During my time at CSUSB, I completed a diverse range of courses, including Software Engineering, Advanced Algorithms, Introduction to Machine Learning, and Artificial Intelligence. These courses provided hands-on experience with various programming languages, such as Python, JavaScript, and C++.
+            </div> 
+            <div>
+                <Button onClick={() =>navigate('/ResumeSite')}>Main Menu</Button>
             </div>
+
         </div>
     );
 };
